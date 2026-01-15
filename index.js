ef57c01882aa59f67c751f3cf291218702040842
Maria <Newspeedartgamer@gmail.com>
2026-01-15 13:33:08 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log("Azumanga daioh");
+let asuka = 100;
+let misato = console.log("input here:");
+console.log(asuka);
+let stuff = "iris";
+console.log("Hello " + stuff);
+
+let num;
+let num1; let num3 = num * num1;
+
+document.getElementById("sumbit1").onclick = function(){num = document.getElementById("mytext1").value; num1 = document.getElementById("mytext2").value; console.log(num * num1); num3 = num * num1;  console.log(num3);} 
+
+
+
+document.getElementById("enter").onclick = function(){let question1 = document.getElementById("swiss")
+if (question1 == "german") { document.getElementById("swissgood").innerHTML = "you answered correctly";}                                                   
+else 
+  {document.getElementById("swissgood").textContent  = "wrong"; }  }  
+ 
+
+
+ 
+
